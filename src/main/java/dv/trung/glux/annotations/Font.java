c2c90277b859ea875248ec15a032a0d0815af711
Trung Dao <trung.dv@geekup.vn>
2020-06-11 13:51:17 +0700

    Change param name to packageName

--- src/main/java/dv/trung/glux/annotations/Font.java
+++ src/main/java/dv/trung/glux/annotations/Font.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Font {
-    String applicationId();
+    String packageName();
 }
